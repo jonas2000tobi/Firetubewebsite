cfe42504228fe251cc840c17c9eb9f696d6c0e7e
jonas2000tobi <39021923+jonas2000tobi@users.noreply.github.com>
2025-10-02 19:56:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,33 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
-
-# Platzhalter-Routen, damit die Menüpunkte anklickbar sind
-@app.route("/guides")
-def guides():
-    return render_template("index.html")
-
-@app.route("/builds")
-def builds():
-    return render_template("index.html")
-
-@app.route("/klassen")
-def klassen():
-    return render_template("index.html")
-
-@app.route("/welt")
-def welt():
-    return render_template("index.html")
-
-@app.route("/tipps")
-def tipps():
+def index():
     return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template("index.html")
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True)
