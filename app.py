09fe606a7a6dce29ba324190f38f60c2ab1e8ebc
jonas2000tobi <39021923+jonas2000tobi@users.noreply.github.com>
2025-10-03 01:16:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")  # optional
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
